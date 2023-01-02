1ec558cd1439f2456d8f37981a4ce1f507f94bdc
Rustam Manafli <rustemmanafov1@gmail.com>
2023-01-03 00:15:23 +0400

    updated

--- AR-kit-simple/ViewController.swift
+++ AR-kit-simple/ViewController.swift
@@ -19,15 +19,37 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        sceneView.autoenablesDefaultLighting = true
+        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+      
+       // let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        addSphereToSene()
     }
+    
+    private func addSphereToSene() {
+        let sphere = SCNSphere(radius: 0.3)
+        sphere.firstMaterial?.diffuse.contents = UIImage(named: "earth.jpg")
+        
+        let sphereNode = SCNNode(geometry: sphere)
+        
+        let rotateAction = SCNAction.rotateBy(x: 0, y: 0.25, z: 0, duration: 2.0)
+        
+        
+        sphereNode.position = SCNVector3(x: 0, y: 0, z: -0.8)
+        sphereNode.runAction(SCNAction.repeatForever(rotateAction))
+        sceneView.scene.rootNode.addChildNode(sphereNode)
+    }
+
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -45,17 +67,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
